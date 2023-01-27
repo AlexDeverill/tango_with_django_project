81f86e7200c53f61ec5422eaf92546f4d50e2e95
Alex Deverill <2652969d@student.gla.ac.uk>
2023-01-27 18:50:59 +0000

    Chapter 9

--- rango/forms.py
+++ rango/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.contrib.auth.models import User
 from rango.models import *
 
 class CategoryForm(forms.ModelForm):
@@ -27,3 +28,15 @@ class PageForm(forms.ModelForm):
     class Meta:
         model = Page
         exclude = ('category',)
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password',)
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ('website', 'picture',)
